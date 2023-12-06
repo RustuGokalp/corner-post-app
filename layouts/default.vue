ce947aebe2f364ab86f8d18ea9494c1a1f558d32
RustuGokalp <94008145+RustuGokalp@users.noreply.github.com>
2023-12-06 14:59:14 +0300

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Header />
+    <Banner />
+    <nuxt />
+  </div>
+</template>
+
+<script>
+import Banner from "~/components/shared/banner";
+import Header from "~/components/shared/header";
+export default {
+  components: {
+    Banner,
+    Header,
+  },
+};
+</script>
+
+<style>
+/* Stil tanımlamaları */
+</style>
