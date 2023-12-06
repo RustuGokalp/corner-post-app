ea4604a7bde9effd170a3b1352d09a02b91710a4
RustuGokalp <94008145+RustuGokalp@users.noreply.github.com>
2023-12-06 16:02:40 +0300

    Create admin and postlist page & admin layout

---
+++ layouts/admin.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Header />
+    <nuxt />
+  </div>
+</template>
+
+<script>
+import Header from "~/components/shared/header";
+export default {
+  components: {
+    Header,
+  },
+};
+</script>
+
+<style>
+/* Stil tanımlamaları */
+</style>
--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,1 +1,20 @@
-<template></template>
+<template>
+  <div>
+    <div class="container d-flex justify-content-center align-items-center">
+      <button class="btn btn-dark">Yeni Bir Yazı Ekle</button>
+    </div>
+    <PostList />
+  </div>
+</template>
+
+<script>
+import PostList from "~/components/post/postList";
+export default {
+  layout: "admin",
+  components: {
+    PostList,
+  },
+};
+</script>
+
+<style scoped></style>
