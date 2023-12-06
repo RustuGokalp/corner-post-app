4487547227c526403d6f59dc741df4e3c161b590
RustuGokalp <94008145+RustuGokalp@users.noreply.github.com>
2023-12-07 00:16:30 +0300

    Make routing in navbar

--- components/shared/header.vue
+++ components/shared/header.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light border-0">
-    <a class="navbar-brand" href="#">Köse Yazısı</a>
+    <nuxt-link class="navbar-brand" to="/" tag="a">Köse Yazısı</nuxt-link>
     <button
       class="navbar-toggler"
       type="button"
@@ -14,16 +14,14 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-        <li class="nav-item active">
-          <a class="nav-link" href="#"
-            >Yazılar <span class="sr-only">(current)</span></a
-          >
-        </li>
+        <nuxt-link class="nav-item" to="/posts" tag="li">
+          <a class="nav-link" href="#">Yazılar</a>
+        </nuxt-link>
       </ul>
       <ul class="navbar-nav my-2 my-lg-0">
-        <li class="nav-item mr-sm-2">
+        <nuxt-link class="nav-item mr-sm-2" to="/admin" tag="li">
           <a class="nav-link" href="#">Admin</a>
-        </li>
+        </nuxt-link>
       </ul>
     </div>
   </nav>
