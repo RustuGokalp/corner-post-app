a60c8518e837583ce82397c7784edcce3f440f77
RustuGokalp <94008145+RustuGokalp@users.noreply.github.com>
2023-12-08 22:29:06 +0300

    Add asyncData in post detail page
    All static writings will be dynamised with BE

--- pages/posts/_postId/index.vue
+++ pages/posts/_postId/index.vue
@@ -1,11 +1,24 @@
 <template>
-  <postDetail />
+  <postDetail :post="fetchedPost" />
 </template>
 <script>
 import postDetail from "~/components/post/postDetail";
 export default {
   components: {
     postDetail,
+  },
+  asyncData(context, callback) {
+    setTimeout(() => {
+      callback(null, {
+        fetchedPost: {
+          id: 1,
+          title: "Trial Title",
+          subtitle: "Trial Subtitle",
+          text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas porta cursus arcu. Mauris sodales tortor non ex semper congue. Nulla.",
+          author: "Gökalp Beğen",
+        },
+      });
+    }, 1000);
   },
 };
 </script>
