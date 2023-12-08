2b77e5a127772c42bd2f9ce26ebb8d5a423f1272
RustuGokalp <94008145+RustuGokalp@users.noreply.github.com>
2023-12-08 21:41:36 +0300

    Add asyncData in posts page

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <PostList />
+    <PostList :posts="fetchedPosts" />
   </div>
 </template>
 
@@ -9,6 +9,28 @@ import PostList from "~/components/post/postList";
 export default {
   components: {
     PostList,
+  },
+  asyncData(context, callback) {
+    setTimeout(() => {
+      callback(null, {
+        fetchedPosts: [
+          {
+            id: 1,
+            title: "Trial Title",
+            subtitle: "Trial Subtitle",
+            text: "Trial Main Text",
+            author: "Rüştü Gökalp Beğen",
+          },
+          {
+            id: 2,
+            title: "Trial Title",
+            subtitle: "Trial Subtitle",
+            text: "Trial Main Text",
+            author: "Rüştü Gökalp Beğen",
+          },
+        ],
+      });
+    }, 1000);
   },
 };
 </script>
