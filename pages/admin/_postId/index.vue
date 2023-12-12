b48fd004b67dea965d13e9809175ad838c7a9944
RustuGokalp <94008145+RustuGokalp@users.noreply.github.com>
2023-12-12 22:43:28 +0300

    Save edited post

--- pages/admin/_postId/index.vue
+++ pages/admin/_postId/index.vue
@@ -1,23 +1,39 @@
 <template>
-  <newPostForm :isUpdate="true" :post="loadedPost" />
+  <newPostForm
+    @submit="updatePost($event)"
+    :isUpdate="true"
+    :post="fetchedPost"
+  />
 </template>
 
 <script>
+import axios from "axios";
 import newPostForm from "~/components/admin/newPostForm";
 export default {
   components: {
     newPostForm,
   },
-  data() {
-    return {
-      loadedPost: {
-        id: 1,
-        title: "Trial Title",
-        subtitle: "Trial Subtitle",
-        text: "Trial Main Text",
-        author: "Rüştü Gökalp Beğen",
-      },
-    };
+  asyncData(context) {
+    return axios
+      .get(
+        `https://my-corner-post-app-default-rtdb.firebaseio.com/posts/${context.params.postId}.json`
+      )
+      .then((response) => {
+        return { fetchedPost: response.data };
+      });
+  },
+  methods: {
+    updatePost(editedPost) {
+      axios
+        .put(
+          `https://my-corner-post-app-default-rtdb.firebaseio.com/posts/${this.$route.params.postId}.json`,
+          editedPost
+        )
+        .then((response) => {
+          this.$router.push("/admin");
+        })
+        .catch((e) => console.log(e));
+    },
   },
 };
 </script>
